c19e0819c3939f495440050f750816f0c2f34f7f
Gomez Kevin <masterk@MasterKiMac.local>
2018-09-01 13:35:41 -0300

    setting deploy

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
-var express = require('express')
-var bodyParser = require('body-parser');
-var app = express()
+/* 
+Setear variables de entorno para NODEjs en CONSOLA:
+WINDOWS: set NODE_ENV=production
+UBUNTU: NODE_ENV=production o export NODE_ENV=production
+*/
+// var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var morgan = require('morgan');
+var cors = require('cors');
+var helmet = require('helmet');
+var app = express();
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
+var bodyParser = require('body-parser');
 
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+// app.use(helmet());
 app.use(bodyParser.json());
+
 
 async function ls() {
     const { stdout, stderr } = await exec('ls');
@@ -13,14 +34,23 @@ async function ls() {
 }
 
 app.post('/', function (req, res) {
-    let headers = req.headers;
-    console.log(headers['x-github-event']);
-    let body = req.body;
-    console.log(body.action)
-    ls();
+  console.log('hola');
+    // let headers = req.headers;
+    // console.log(headers['x-github-event']);
+    // let body = req.body;
+    // console.log(body.action)
+    // ls();
     res.send('OK')
 })
 
-app.listen(8080, function () {
-    console.log('listening on port 8080')
-})
+app.get('/', function(req, res) {
+  res.send('hello world');
+});
+
+module.exports = app;
+
+
+
+
+
+
